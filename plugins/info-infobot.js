8ac63678c0afb9da95d846484f643822ad630415
JhonatanPpp <119618637+JhonatanPpp@users.noreply.github.com>
2023-05-07 12:50:24 -0500

    Update info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -21,7 +21,7 @@ let speed = neww - old
 let info = `
 ╠═〘 𝐈𝐍𝐅𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 〙 ═
 ╠
-╠➥ [🤴🏻] 𝙲𝚁𝙴𝙰𝙳𝙾𝚁: *MIN*
+╠➥ [🤴🏻] 𝙲𝚁𝙴𝙰𝙳𝙾𝚁𝙰: *MIN*
 ╠➥ [#️⃣] 𝙽𝚄𝙼𝙴𝚁𝙾: *+51 921 934 527*
 ╠➥ [🎳] 𝙿𝚁𝙴𝙵𝙸𝙹𝙾: *${usedPrefix}*
 ╠➥ [🔐] 𝙲𝙷𝙰𝚃𝚂 𝙿𝚁𝙸𝚅𝙰𝙳𝙾𝚂: *${chats.length - groups.length}*
@@ -40,7 +40,7 @@ let info = `
 ╠➥ [👨‍🦯] 𝚅𝙴𝙻𝙾𝙲𝙸𝙳𝙰𝙳: 
 ╠  *${speed} ms* 
 ╠
-╠═〘 MIN 〙 ═
+╠═〘 MIN BOT 〙 ═
 `.trim() 
 let aa = { quoted: m, userJid: conn.user.jid }
 let res = generateWAMessageFromContent (m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: info, secuenceNumber: "0", contextInfo: {mentionedJid: conn.parseMention()}}}, aa)
